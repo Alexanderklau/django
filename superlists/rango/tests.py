bddee85a3d9a0970b9739d82868e21918b5a9275
Alexanderlau <429095816@qq.com>
2016-09-05 02:15:03 +0800

    2016

--- superlists/rango/tests.py
+++ superlists/rango/tests.py
@@ -25,6 +25,11 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_homr_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func,home_page)
+    def test_home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(),0)
+
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
@@ -40,6 +45,11 @@ class HomePageTest(TestCase):
         request.method = 'POST'
         request.POST['item_text'] = 'A new list item'
         response = home_page(request)
+
+        self.assertEqual(Item.objects.count(),1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
         self.assertIn('A new list item',response.content.decode())
         expected_html = render_to_string(
             'home.html',
--- superlists/rango/views.py
+++ superlists/rango/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Item
 def home_page(request):
+    if request.method == 'POST':
+        new_item_text = request.POST['item_text']
+        Item.objects.create(text=new_item_text)
+    else:
+        new_item_text = ''
     return render(request,'home.html',{
-        'new_item_text':request.POST.get('item_text',''),
+        'new_item_text':new_item_text,
     })
+    # item = Item()
+    # item.text = request.POST.get('item_text','')
+    # item.save()
+    # return render(request,'home.html',{
+    #     'new_item_text':item.text
+    #     'new_item_text':request.POST.get('item_text','')
+    # })
+    # return render(request,'home.html',{
+    #     'new_item_text':request.POST.get('item_text',''),
+    # })
     #return HttpResponse('<html><title>To-Do lists</title></html>')
 # Create your views here.
