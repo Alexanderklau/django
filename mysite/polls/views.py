24fbd30856cea4dbb7cb5edbc1b7f0380e2217e9
Alexanderlau <429095816@qq.com>
2016-11-03 16:24:26 +0800

    flask

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def results(requeset,question_id):
+    response = "You are looking at the results of question %s"
+    return HttpResponse(response % question_id)
+
+def vote(request,question_id):
+    return HttpResponse("You are voting on question %s" % question_id)
+
+def detail(request,question_id):
+    return HttpResponse("You are looking at question %s" % question_id )
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 # Create your views here.
