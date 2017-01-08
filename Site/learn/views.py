3f318c40b8c0a89f2b51e5a316c465d532758d76
Alexanderlau <429095816@qq.com>
2017-01-08 16:33:05 +0800

    spider

--- Site/learn/views.py
+++ Site/learn/views.py
@@ -1,4 +1,5 @@
 #coding:utf-8
+from __future__ import unicode_literals
 import json
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -12,10 +13,18 @@ def add2(request,a,b):
     return HttpResponse(str(c))
 def home(request):
     List = [u'哈哈','JSON']
+    string = u"fuck U!"
+    number = map(str,range(100))
+    info_dict = {'site':u'nixe','content':u'IT'}
+    example = ["Html","css","jquery"]
     Dict = {'site':u'啊哈哈','author':'alex'}
     return render(request,'home.html',{
+        'info_dict':info_dict,
+        'example':example,
         'List': json.dumps(List),
-        'Dict': json.dumps(Dict)
+        'Dict': json.dumps(Dict),
+        'string':string,
+        'number':number,
     })
     # string = u"Fuck You!"
     # Tutorlisad = ["Html","css","JQuery","Python"]
