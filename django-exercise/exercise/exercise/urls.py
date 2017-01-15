f4821043a1e520f12c633636ff898cbba759844f
Alexanderlau <429095816@qq.com>
2017-01-15 23:20:12 +0800

    spider

--- django-exercise/exercise/exercise/urls.py
+++ django-exercise/exercise/exercise/urls.py
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url,include 
+from django.conf.urls import url,include,patterns
 from django.contrib import admin
 # from text import views
 
-urlpatterns = [
+urlpatterns = patterns('',
     url(r'^admin/', admin.site.urls),
     url(r'hello/$','text.views.hello',name='text')
-]
+)
