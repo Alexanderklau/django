cc67c8ecf8137e6e33141e5d7cb3a707a08bbfb1
Alexanderlau <429095816@qq.com>
2017-01-23 00:05:14 +0800

    django

--- django-exercise/exercise/text/admin.py
+++ django-exercise/exercise/text/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import *
+admin.site.register(Publiser)
+admin.site.register(Human)
+admin.site.register(HumanDetail)
+admin.site.register(Work)
 # Register your models here.
--- django-exercise/exercise/text/models.py
+++ django-exercise/exercise/text/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
+class Publiser(models.Model):
+    company_name = models.CharField("公司名",max_length=30)
+    company_address = models.CharField(max_length=60)
+    company_city = models.CharField(max_length=50)
+    company_web = models.URLField()
+    class Meta:
+        verbose_name = '公司信息'
+        verbose_name_plural = verbose_name
+
+
+class Human(models.Model):
+    name = models.CharField(max_length=30)
+
+    class Meta:
+        verbose_name = '姓名'
+        verbose_name_plural = verbose_name
+
+class HumanDetail(models.Model):
+    sex = models.BooleanField(max_length=1,choices=((0,'男'),(1,'女'),))
+    email = models.EmailField()
+    address = models.CharField(max_length=50)
+    birthady = models.DateField()
+    human = models.OneToOneField(Human)
+
+    class Meta:
+        verbose_name = '人员信息'
+        verbose_name_plural = verbose_name
+
+class Work(models.Model):
+    Work_name = models.CharField(max_length=100)
+    Work_human = models.ManyToManyField(Human)
+    Work_company = models.ForeignKey(Publiser)
+    Work_date = models.DateField()
+
+    class Meta:
+        verbose_name = '工作信息'
+        verbose_name_plural = verbose_name
+
+
 # Create your models here.
