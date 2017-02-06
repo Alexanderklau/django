f5ab0b95b7e8f47a63ed46179b174f3fad360f7a
Alexanderlau <429095816@qq.com>
2017-02-06 12:51:19 +0800

    django

--- Blog_DEMO/Blog/views.py
+++ Blog_DEMO/Blog/views.py
@@ -3,6 +3,8 @@ import logging
 from django.conf import settings
 from .models import *
 from django.core.paginator import Paginator,InvalidPage,EmptyPage,PageNotAnInteger
+from django.db import connection
+
 logger = logging.getLogger('blog.views')
 
 def global_setting(request):
@@ -21,6 +23,17 @@ def index(request):
             article_list = paginator.page(page)
         except (EmptyPage,InvalidPage,PageNotAnInteger):
             article_list = paginator.page(1)
+            archive_list = Article.objects.distinct_date()
+    #         cursor = connection.cursor
+    #         cursor.execute('SELECT DISCONNECT DATA_FORMAT(date_publish,"%%Y-%%m") as col_date FROM blog_article ORDER BY date_publish;')
+    #         ros = cursor.fetchall()
+    #         print(ros)
+    # #     文章归档
+    # # 先找到所有的年份-月份，利用sql找出
+    # #     archive_list = Article.objects.raw('SELECT id,DATA_FORMAT(date_publish,"%%Y-%%m") as col_date FROM blog_article ORDER BY date_publish')
+    # #     for artivle in archive_list:
+    # #         print(artivle)
+
     except Exception as e:
         pass
     return render(request,'index.html',locals())
