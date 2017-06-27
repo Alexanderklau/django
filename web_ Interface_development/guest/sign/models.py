ec71434f8f33b58f798f305bb313288799045f16
alexanderklau <429095816@qq.com>
2017-06-27 13:40:18 +0800

    add login

--- web_ Interface_development/guest/sign/models.py
+++ web_ Interface_development/guest/sign/models.py
@@ -1,3 +1,9 @@
+#-*- coding:utf-8 -*-
 from django.db import models
+
+class Event(models.Model):
+    name = models.CharField(max_length=100,help_text='姓名')
+    limit = models.IntegerField(help_text='参加人数')
+    status = models.BooleanField()
 
 # Create your models here.
