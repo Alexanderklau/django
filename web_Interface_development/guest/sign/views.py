d7cf3559852dd2b3f52429ac127c877f4b7bf073
Alexanderklau <429095816@qq.com>
2017-06-29 22:14:12 +0800

    add

--- web_Interface_development/guest/sign/views.py
+++ web_Interface_development/guest/sign/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
 from django.contrib import auth
-from sign.models import Event
+from sign.models import Event,Guest
 from django.contrib.auth.decorators import login_required
 
 def index(request):
@@ -24,6 +24,19 @@ def event_manage(request):
     event_list = Event.objects.all()
     username = request.session.get('user','')
     return render(request,"event_manage.html",{"user":username,"events":event_list})
+
+@login_required
+def search_name(request):
+    username = request.session.get('user',"")
+    search_name = request.GET.get('name',"")
+    event_list = Event.objects.filter(name__contains = search_name)
+    return render(request,"event_manage.html",{"user": username,"events": event_list})
+@login_required
+def guest_manage(request):
+    username = request.session.get('user','')
+    guest_list = Guest.objects.all()
+    return render(request,"guest_manage.html",{"user": username,"guests":guest_list})
+
 
 # Create your views here.
 
